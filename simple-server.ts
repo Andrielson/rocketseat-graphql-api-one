48f8a9fe45fd17ff91b2052c17105336ccb8b529
Andrielson Silva <andrielson@gmail.com>
2022-10-22 17:37:40 -0300

    add helloWorld query

---
+++ simple-server.ts
@@ -0,0 +1,23 @@
+import {ApolloServer, gql} from "apollo-server";
+
+
+const typeDefs = gql`
+    type Query {
+        helloWorld: String!
+    }
+    
+    # type Mutation {}
+`;
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers: {
+        Query: {
+            helloWorld: () => 'Hello World!'
+        }
+    }
+});
+
+server.listen().then(({url}) => {
+    console.log(`🚀 HTTP server running on ${url}...`);
+});
